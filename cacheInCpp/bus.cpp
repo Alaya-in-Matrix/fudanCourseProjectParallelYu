a6216ef9dcc167aaf2f749eba1a4c4f5e848da61
Lv Wenlong <lvwenlong_lambda@qq.com>
2014-12-14 00:35:17 +0800

    indentation

--- cacheInCpp/bus.cpp
+++ cacheInCpp/bus.cpp
@@ -6,13 +6,13 @@
 #include"global_def.h"
 
 CacheBus::CacheBus(){
-	//caches = new Cache[CORENUM];
-	for(int i=0;i<CORENUM;i++)
-		caches.push_back(NULL);
+    //caches = new Cache[CORENUM];
+    for(int i=0;i<CORENUM;i++)
+        caches.push_back(NULL);
 };
 CacheBus::~CacheBus(){
-	for(int i=0;i<CORENUM;i++)
-		setNull(i);
+    for(int i=0;i<CORENUM;i++)
+        setNull(i);
 };
 void CacheBus::setNull(int idx){caches[idx] = NULL;}
 void CacheBus::addCache(int idx,Cache* c){caches[idx]=c;}
@@ -21,31 +21,31 @@ void CacheBus::broadcast(int cIdx,broadCastMsg msg,unsigned short blockAddress,s
     for(int i = 0; i < caches.size(); i++)
     {
         Cache* it = caches[i];
-		bool fuck = it->hit(blockAddress);
+        bool fuck = it->hit(blockAddress);
         if(it!=NULL &&it->hit(blockAddress) && i != cIdx)//has this block
         {
             status s = it->getStatus(blockAddress);
             switch(s)
             {
-			case status::INVALID:
-				break;
-			case status::SHARED:
-				if(msg == broadCastMsg::WRITE_MISS || msg == broadCastMsg::INVALIDATE)
-				{
-					it->set_status(blockAddress,status::INVALID);
-				}
-				break;
-			case status::MODIFIED:
-				assert(msg != broadCastMsg::INVALIDATE);
-				if(msg == broadCastMsg::READ_MISS)
-				{
-					it->set_status(blockAddress,status::SHARED);
-				}
-				else if(msg == WRITE_MISS)
-				{
-					it->set_status(blockAddress,status::INVALID);
-				}
-				it->write_back(blockAddress);
+                case status::INVALID:
+                    break;
+                case status::SHARED:
+                    if(msg == broadCastMsg::WRITE_MISS || msg == broadCastMsg::INVALIDATE)
+                    {
+                        it->set_status(blockAddress,status::INVALID);
+                    }
+                    break;
+                case status::MODIFIED:
+                    assert(msg != broadCastMsg::INVALIDATE);
+                    if(msg == broadCastMsg::READ_MISS)
+                    {
+                        it->set_status(blockAddress,status::SHARED);
+                    }
+                    else if(msg == WRITE_MISS)
+                    {
+                        it->set_status(blockAddress,status::INVALID);
+                    }
+                    it->write_back(blockAddress);
                     break;
                 default:
                     assert(ERROR_STATUS);
