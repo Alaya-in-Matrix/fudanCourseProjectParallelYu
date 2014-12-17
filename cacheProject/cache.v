1eaec573eeb934fb20a5404c32e15fb341b4be90
lvwenlong <lvwenlong_lambda@qq.com>
2014-12-17 14:27:00 +0800

    cache:backup for sync

--- cacheProject/cache.v
+++ cacheProject/cache.v
@@ -1,7 +1,7 @@
 `include global_def.v
 module cache(clk,
              cpuRW, cpuAddr, cpuDataIn, cpuDataOut,cpuSuccess,
-             memRW,memAddr,memDataOut,memDataIn,memSuccess,
+             memRW,memAddr,memDataOut,memDataIn,memNotBusy,
              snoopInAction,snoopInProc,snoopInAddr,snoopInValue,
              snoopOutAction,snoopOutProc,snoopOutAddr,snoopOutValue);
 //clock
@@ -19,39 +19,56 @@ output reg memRW;
 output reg [ADDRESSBIT-1:0] memAddr;
 output reg [WORDSIZE*BLOCKBYTE-1:0] memDataOut;
 input [WORDSIZE*BLOCKBYTE-1:0] memDataIn;
-input memSuccess;
+input memNotBusy;
 
 input [3:0] snoopInAction;
-input snoopInProc;
 input [ADDRESSBIT-1:0] snoopInAddr;
 input [WORDSIZE-1:0] snoopInValue;
 
 output reg[3:0] snoopOutAction;
-output reg snoopOutProc;
 output reg[ADDRESSBIT-1:0] snoopOutAction;
 output reg[WORDSIZE-1:0] SnoopOutValue;
 
 //simple direct map cache
-reg[ADDRESSBIT-1:0] blockAddr[CACHELINENUM-1:0];
+reg[ADDRESSBIT-1:0] cacheLineBlockAddr[CACHELINENUM-1:0];
 reg[1:0] status[CACHELINENUM-1:0];
 reg[BLOCKBYTE*WORDSIZE-1:0] cacheLine[CACHELINENUM-1:0];
 
+wire[BLOCKADDRBIT-1:0] blockAddr       = cpuAddr[(ADDRESSBIT-1)-:BLOCKADDRBIT];
+wire[OFFSETADDRBIT-1:0] offsetAddr     = cpuAddr[0+:OFFSETADDRBIT];
+wire[CACHELINEIDXBIT-1:0] cacheLineIdx = blockAddr % CACHELINENUM;
+
+wire [1:0] hitmiss;
+assign hitmiss[1] = (cpuRW == RD) ? 0 : 1;
+assign hitmiss[0] = (blockAddr == cacheLineBlockAddr[cacheLineIdx]) ? 0 : 1;
+
 always @(posedge clk)
 begin
-    //是不是可以试一下生成块呢
-    //大键似乎还是有一点肉, 不过比ikbc要好一点.
-    for(i=0; i<CACHELINENUM; i=i+1)
-    begin
-        case (status[i])
-            MODIFIED:
-                $display(1);
-            SHARED:
-                $display(1);
-            INVALID:
-                $display(1);
-            default:
-                $display("error cache line status");
-        endcase
-    end
+    case(status[cacheLineIdx])
+        MODIFIED:
+            case (hitmiss)
+                READHIT:
+                    cpuDataOut = cacheLine[cacheLineIdx][offset];
+                WRITEHIT:
+                    //TODO: 这个只会有one bit!
+                    cacheLineIdx[cacheLineIdx][offset] = cpuDataIn;
+                READMISS:
+                begin
+                    status[cacheLineIdx] = SHARED;
+                    snoopOutAction = MSG_READMISS;
+                    snoopOutAddr = blockAddr;
+                end
+                WRITEMISS:
+                begin
+                    if(memNotBusy)
+                    begin
+                        //write back memory
+                    end
+                    snoopOutAction = MSG_WRITEMISS;
+                end
+        SHARED:
+        INVALID:
+        default:
+    endcase
 end
 endmodule
--- cacheProject/global_def.v
+++ cacheProject/global_def.v
@@ -2,9 +2,14 @@
 `define REGISTERNUM  32      //32 registers per processor
 `define WORDSIZE  8       //8 bit per register
 `define BLOCKBYTE    8       //8 byte per block
-`define CACHELINENUM 16      //cache size: 16blocks * 8b/block = 128B
 `define MEMSIZE      65536   //memory size 4KB
+
+`define CACHELINENUM 16      //cache size: 16blocks * 8b/block = 128B
+`define CACHELINEIDXBIT 4
+
 `define ADDRESSBIT   16      //16bit address
+`define OFFSETADDRBIT 3
+`define BLOCKADDRBIT 13
 
 
 // indicate whether a cache want to read or write memory
@@ -15,8 +20,18 @@
 `define ACCESSING 0
 `define ACCESS_SUCCESS 1
 
-`define SNOOPMSGWIDTH 16
 
 `define MODIFIED 2'b00
 `define SHARED   2'b01
 `define INVALID  2'b10
+
+`define READHIT 2'b00
+`define READMISS 2'b01
+`define WRITEHIT 2'b10
+`define WRITEMISS 2'b11
+
+
+//snoop message
+`define SNOOPACTIONWIDTH 4
+`define MSG_READMISS 4'h0
+`define MSG_WRITEMISS 4'h1
